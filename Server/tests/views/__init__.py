a05c0068a7ce7eb713aa69fae223ba932cb480f8
JoMingyu <city7310@naver.com>
2018-04-17 14:10:24 +0900

    UPDATE: TCBase, Argument parser

--- Server/tests/views/__init__.py
+++ Server/tests/views/__init__.py
@@ -8,9 +8,9 @@ from app import app
 
 class TCBase(TC):
     def __init__(self, *args, **kwargs):
-        TC.__init__(self, *args, **kwargs)
-
         self.client = app.test_client()
+
+        super(TCBase, self).__init__(*args, **kwargs)
 
     def _create_fake_account(self):
         pass
@@ -26,58 +26,50 @@ class TCBase(TC):
     def tearDown(self):
         pass
 
-    def json_request(self, method, target_url_rule, query_string=None, data=None, token=None, *args, **kwargs):
+    def json_request(self, method, target_url_rule, token=None, *args, **kwargs):
         """
         Helper for json request
 
         Args:
             method (func): Request method
             target_url_rule (str): URL rule for request
-            query_string (dict): Query parameters
-            data (dict): JSON payload (application/json)
             token (str) : JWT or OAuth's access token with prefix(Bearer, JWT, ...)
 
         Returns:
             Response
         """
-        if token is None:
-            token = self.access_token
+        data = kwargs.pop('data')
 
         return method(
             target_url_rule,
-            query_string=query_string,
             data=ujson.dumps(data) if data else None,
             content_type='application/json',
-            headers={'Authorization': token},
+            headers={'Authorization': token or self.access_token},
             *args,
             **kwargs
         )
 
-    def request(self, method, target_url_rule, query_string=None, data=None, token=None, *args, **kwargs):
+    def request(self, method, target_url_rule, token=None, *args, **kwargs):
         """
         Helper for common request
 
         Args:
             method (func): Request method
             target_url_rule (str): URL rule for request
-            query_string (dict): Query parameters
-            data (dict): Body parameters (application/x-www-form-urlencoded)
             token (str) : JWT or OAuth's access token with prefix(Bearer, JWT, ...)
 
         Returns:
             Response
         """
-        if token is None:
-            token = self.access_token
-
         return method(
             target_url_rule,
-            query_string=query_string,
-            data=data,
-            headers={'Authorization': token},
+            headers={'Authorization': token or self.access_token},
             *args,
             **kwargs
         )
 
+    def decode_response_data(self, resp):
+        return resp.data.decode()
+
     def get_response_data(self, resp):
-        return ujson.loads(resp.data.decode())
+        return ujson.loads(self.decode_response_data(resp))
