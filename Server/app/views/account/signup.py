6c6bf1b1af436e50141c319060dd75b5fa70c16e
JoMingyu <city7310@naver.com>
2018-05-08 14:22:46 +0900

    DOING: POST /initialize/info

--- Server/app/views/account/signup.py
+++ Server/app/views/account/signup.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Blueprint, Response, abort, g, request
 from flask_restful import Api
 from werkzeug.security import generate_password_hash
@@ -42,8 +44,35 @@ class Signup(BaseResource):
 
             return Response('', 201)
 
+
+@api.resource('/initialize/info')
 class InitializeInfo(BaseResource):
+    @json_required({'id': str, 'shortestCycle': int, 'longestCycle': int, 'latestMensStartDate': str})
     def post(self):
         """
         기본 정보 업로드
         """
+        id = request.json['id']
+        shortest_cycle = request.json['shortestCycle']
+        longest_cycle = request.json['longestCycle']
+        latest_mens_start_date_str = datetime.strptime(request.json['latestMensStartDate'], '%Y-%m-%d')
+
+        account = AccountBase.objects(id=id).first()
+
+        if not account:
+            abort(400)
+
+        # TODO 이미 채워져 있는 경우에도 response 하나 있어야 함
+
+        if shortest_cycle > longest_cycle:
+            return {
+                'msg': 'cycle'
+            }
+
+        if 26 <= shortest_cycle <= longest_cycle <= 32:
+            # 표준일 피임법
+            pass
+        else:
+            # 리듬피임법(크나우스 오기노법)
+            pass
+
