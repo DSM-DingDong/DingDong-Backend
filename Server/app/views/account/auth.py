0095a57e0111876009f2b0639677d14100ec2d76
JoMingyu <city7310@naver.com>
2018-05-08 21:54:25 +0900

    TEMP

--- Server/app/views/account/auth.py
+++ Server/app/views/account/auth.py
@@ -1,17 +1,30 @@
 from flask import Blueprint, Response, abort, g, request
 from flask_restful import Api
 from flasgger import swag_from
+from werkzeug.security import check_password_hash
 
+from app.models.account import SystemAccountModel, FacebookAccountModel
 from app.views import BaseResource, auth_required, json_required
 
 api = Api(Blueprint(__name__, __name__))
 
 
 class Auth(BaseResource):
+    @json_required({'id': str, 'pw': str})
     def post(self):
         """
         자체 계정 로그인
         """
+        id = request.json['id']
+        pw = request.json['pw']
+
+        account = SystemAccountModel.objects(id=id).first()
+
+        if not account:
+            abort(401)
+        else:
+            # TODO
+            pass
 
 
 class FacebookAuth(BaseResource):
