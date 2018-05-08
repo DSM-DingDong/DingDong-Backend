a635debdcff4580799c67ff46b4feef38750bab6
JoMingyu <city7310@naver.com>
2018-05-08 13:42:49 +0900

    UPDATE: 계정 스키마 변경

--- Server/app/models/account.py
+++ Server/app/models/account.py
@@ -1,16 +1,43 @@
+from datetime import datetime
+
 from app.models import *
 
 
-class AccountModel(Document):
+class AccountBase(Document):
     meta = {
-        'collection': 'account',
+        'abstract': True,
+        'allow_inheritance': True
     }
+
+    signup_time = DateTimeField(
+        required=True,
+        default=datetime.now()
+    )
 
     id = StringField(
         primary_key=True
     )
 
+    shortest_cycle = IntField()
+    longest_cycle = IntField()
+    latest_mens_start_date = DateTimeField()
+
+    predicted_next_mens_start_date = DateTimeField()
+
+
+class SystemAccountModel(AccountBase):
+    meta = {
+        'collection': 'system_account'
+    }
+
     pw = StringField()
+
+
+class FacebookAccountModel(AccountBase):
+    meta = {
+        'collection': 'facebook_account'
+    }
+
     connected_sns = StringField()
 
 
@@ -26,6 +53,6 @@ class RefreshTokenModel(Document):
     pw_snapshot = StringField()
 
     owner = ReferenceField(
-        document_type=AccountModel,
+        document_type=AccountBase,
         required=True
     )
