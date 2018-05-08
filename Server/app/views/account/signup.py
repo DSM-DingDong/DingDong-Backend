10e20ff82cf02a7e740f79f8133ce83025c30c3d
JoMingyu <city7310@naver.com>
2018-05-08 14:21:32 +0900

    DONE: GET /check/id/<id>

--- Server/app/views/account/signup.py
+++ Server/app/views/account/signup.py
@@ -1,17 +1,25 @@
 from flask import Blueprint, Response, abort, g, request
 from flask_restful import Api
-from flasgger import swag_from
+from werkzeug.security import generate_password_hash
 
+from app.models.account import AccountBase, SystemAccountModel
 from app.views import BaseResource, auth_required, json_required
 
 api = Api(Blueprint(__name__, __name__))
 
 
+@api.resource('/check/id/<id>')
 class IDCheck(BaseResource):
-    def get(self):
+    def get(self, id):
         """
-        ID 중복체크
+        자체 계정 ID 중복체크
         """
+        if SystemAccountModel.objects(id=id):
+            # 중복됨
+            abort(409)
+        else:
+            # 중복되지 않음
+            return Response('', 200)
 
 
 class Signup(BaseResource):
