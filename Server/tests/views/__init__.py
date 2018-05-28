d11d40250e94e536fe725fd8976c92070ac207b5
JoMingyu <city7310@naver.com>
2018-05-29 08:53:01 +0900

    [DONE] _generate_tokens()

--- Server/tests/views/__init__.py
+++ Server/tests/views/__init__.py
@@ -47,10 +47,11 @@ class TCBase(TC):
 
     def _generate_tokens(self):
         with app.app_context():
-            self.primary_user_access_token = None
-            self.primary_user_refresh_token = None
-            self.secondary_user_access_token = None
-            self.secondary_user_refresh_token = None
+            self.primary_user_access_token = create_access_token(TokenModel.generate_token(AccessTokenModel, self.primary_user))
+            self.primary_user_refresh_token = create_refresh_token(TokenModel.generate_token(RefreshTokenModel, self.primary_user))
+
+            self.secondary_user_access_token = create_access_token(TokenModel.generate_token(AccessTokenModel, self.secondary_user))
+            self.secondary_user_refresh_token = create_refresh_token(TokenModel.generate_token(RefreshTokenModel, self.secondary_user))
 
     def setUp(self):
         self.primary_user_id = 'primary_user'
