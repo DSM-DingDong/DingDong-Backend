431811e50e094b845a7d2805857b1c7f64ed3834
JoMingyu <city7310@naver.com>
2018-06-11 17:14:13 +0900

    [REMOVE] run_test.py, utils 패키지

--- Server/run_test.py
+++
@@ -1,9 +0,0 @@
-import unittest as ut
-
-from app import app
-
-if __name__ == '__main__':
-    app.testing = True
-
-    all_tests = ut.TestLoader().discover('tests', '*.py')
-    ut.TextTestRunner().run(all_tests)
